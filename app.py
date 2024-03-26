146dc8449a07e1cc4f6acc401c99c6b4186ce1c7
Molly Siebenaler <112716040+mollysiebenaler@users.noreply.github.com>
2024-03-26 15:01:49 -0500

    wip- partner change

---
+++ app.py
@@ -0,0 +1,17 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route("/")
+def index():
+    return 'hello world, index'
+
+@app.route("/molly")
+def molly():
+    n1 = 10
+    n2 = 20
+    return str(sum)
+
+if __name__ == "__main__":
+    app.run(debug=True)
+
